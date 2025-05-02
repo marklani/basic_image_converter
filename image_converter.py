4f0611c7585782319aa6a2427299d8db179bde8d
marklani <106664365+marklani@users.noreply.github.com>
2025-05-02 18:55:36 +0800

    Update image_converter.py
    Refactored to use object classes.
    Make use of StringVar updating values instead of using events.

--- image_converter.py
+++ image_converter.py
@@ -3,79 +3,87 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
 from pathlib import Path
-from os import path
 
-
-image_format_to_extension={
+IMAGE_FORMAT_TO_EXTENSION={
     'jpeg' : 'jpeg',
     'png' : 'png',
     'jpeg2000' : 'jp2',
     'gif' : 'GIF',
     'bmp' : 'BMP',
+    'tiff' : 'TIF',
+    'ico' : 'ICO',
+    'dib' : 'DIB'
 }
 
-def convert_image_to_grayscale():
+def convert_image_to_grayscale(filepath, image_format, save_dir, result_path_label_text_variable):
     try:
-        filepath = selected_file_label['text']
-        image_format = image_type_combobox.get()
-        new_filedir = selected_save_path_label['text']
-
+        file_extension = Path(filepath).suffix
+        if str(file_extension).lower() == '.eps':
+            raise Exception(f'Image conversion failed: {file_extension} images cannot be opened.')
         jpeg_image = Image.open(open(filepath,'rb')).convert('L')
         filename_without_extension = Path(filepath).stem
-        new_filename = f'{filename_without_extension}.{image_format_to_extension[image_format.lower()]}'
-        new_filepath = path.join(new_filedir, new_filename)
+        new_filename = f'{filename_without_extension}.{IMAGE_FORMAT_TO_EXTENSION[image_format.lower()]}'
+        new_filepath = Path(save_dir, new_filename)
         jpeg_image.save(new_filepath, image_format)
-        result_path_label['text'] = f'Image successfully converted at: {new_filepath}'
+        result_path_label_text_variable.set(f'Image successfully converted at: {new_filepath}')
     except Exception as e:
-        print(f'Error: {e}')
+        result_path_label_text_variable.set(f'Error: {e}')
 
-def select_file_dialog():
+def select_file_dialog(selected_file_label_text_variable):
     filepath = filedialog.askopenfilename()
     if filepath:
-        selected_file_label.configure(text=filepath)
+        selected_file_label_text_variable.set(filepath)
 
-def select_save_filename_dialog():
-    filepath = filedialog.askdirectory()
-    if filepath:
-        selected_save_path_label.configure(text=filepath)
+def select_save_dir_dialog(selected_save_dir_label_text_variable):
+    save_dir = filedialog.askdirectory()
+    if save_dir:
+        selected_save_dir_label_text_variable.set(save_dir)
 
-window = tk.Tk()
-window.title("Image Converter")
+class CustomFrame(tk.Frame):
+    def __init__(self, master, pack_side):
+        super().__init__(master)
+        super().pack(side=pack_side)
 
+class CustomLabel(tk.Label):
+    def __init__(self, master, pack_side, default_text=''):
+        self.text_variable = tk.StringVar(value=default_text)
+        super().__init__(master, textvariable=self.text_variable)
+        super().pack(side=pack_side)
 
-top_frame = tk.Frame(window)
-top_frame.pack(side=tk.TOP)
-mid_frame = tk.Frame(window)
-mid_frame.pack(side=tk.TOP)
-upper_mid_frame = tk.Frame(window)
-upper_mid_frame.pack(side=tk.TOP)
-bottom_frame = tk.Frame(window)
-bottom_frame.pack(side=tk.TOP)
+class CustomButton(tk.Button):
+    def __init__(self, master, pack_side, default_text='', command=None):
+        self.text_variable = tk.StringVar(value=default_text)
+        super().__init__(master, textvariable=self.text_variable, command=command)
+        super().pack(side=pack_side)
 
-selected_file_label = tk.Label(top_frame, text="Choose an image to convert.")
-selected_file_label.pack(side=tk.LEFT)
+class CustomCombobox(ttk.Combobox):
+    def __init__(self, master, pack_side, values, default_text=''):
+        self.text_variable = tk.StringVar(value=default_text)
+        super().__init__(master, textvariable=self.text_variable, values=values, state='readonly')
+        super().pack(side=pack_side)
 
-select_file_button = tk.Button(top_frame, text="Open", command=select_file_dialog)
-select_file_button.pack(side=tk.LEFT)
+def main_window():
+    window = tk.Tk()
+    window.title("Image Converter")
 
-selected_image_type_label = tk.Label(upper_mid_frame, text="Choose image type to convert to.")
-selected_image_type_label.pack(side=tk.LEFT)
+    file_selection_frame = CustomFrame(window, tk.TOP)
+    selected_file_label = CustomLabel(file_selection_frame, tk.LEFT, "Choose an image to convert")
+    select_file_button = CustomButton(file_selection_frame, tk.LEFT, "Open", lambda: select_file_dialog(selected_file_label.text_variable))
 
-default_combo_box_text = tk.StringVar(value='Image type')
-combo_box_values = ['jpeg', 'png', 'jpeg2000', 'gif', 'bmp']
-image_type_combobox = ttk.Combobox(upper_mid_frame, textvariable=default_combo_box_text, values=combo_box_values, state='readonly')
-image_type_combobox.pack(side=tk.LEFT)
+    type_selection_frame = CustomFrame(window, tk.TOP)
+    selected_image_type_label = CustomLabel(type_selection_frame, tk.LEFT, "Choose image type to convert to.")
+    image_type_combobox = CustomCombobox(type_selection_frame, tk.LEFT, list(IMAGE_FORMAT_TO_EXTENSION.keys()), "Image type")
 
-selected_save_path_label = tk.Label(mid_frame, text='Select a path to save the converted image.')
-selected_save_path_label.pack(side=tk.LEFT)
+    save_dir_selection_frame = CustomFrame(window, tk.TOP)
+    selected_save_dir_label = CustomLabel(save_dir_selection_frame, tk.LEFT, "Select a path to save the converted image.")
+    select_save_dir_button = CustomButton(save_dir_selection_frame, tk.LEFT, "Save to...", lambda: select_save_dir_dialog(selected_save_dir_label.text_variable))
 
-select_save_path_button = tk.Button(mid_frame, text="Save to...", command=select_save_filename_dialog)
-select_save_path_button.pack(side=tk.LEFT)
+    convert_image_frame = CustomFrame(window, tk.TOP)
+    result_path_label = CustomLabel(convert_image_frame, tk.BOTTOM)
+    convert_image_button = CustomButton(convert_image_frame, tk.TOP, 'Convert!', lambda: convert_image_to_grayscale(selected_file_label.text_variable.get(), image_type_combobox.get(), selected_save_dir_label.text_variable.get(), result_path_label.text_variable))
 
-convert_image_button = tk.Button(bottom_frame, text='Convert!', command=convert_image_to_grayscale)
-convert_image_button.pack(side=tk.TOP)
+    return window
 
-result_path_label = tk.Label(bottom_frame)
-result_path_label.pack(side=tk.TOP)
-
-window.mainloop()
+if __name__ == "__main__":
+    window = main_window()
+    window.mainloop()
